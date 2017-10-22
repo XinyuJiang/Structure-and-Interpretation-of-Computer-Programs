4eb68180fcd6fe674d7dfb0a47f89171efdd6fa4
XinyuJiang <503273921@qq.com>
2017-10-21 17:17:58 -0700

    labs

---
+++ lab08/lab08.py
@@ -0,0 +1,203 @@
+def deep_len(lnk):
+    """ Returns the deep length of a possibly deep linked list.
+    >>> deep_len(Link(1, Link(2, Link(3))))
+    3
+    >>> deep_len(Link(Link(1, Link(2)), Link(3, Link(4))))
+    4
+    >>> levels = Link(Link(Link(1, Link(2)), \
+            Link(3)), Link(Link(4), Link(5)))
+    >>> print(levels)
+    <<<1 2> 3> <4> 5>
+    >>> deep_len(levels)
+    5
+    """
+    "*** YOUR CODE HERE ***"
+    if lnk is Link.empty:
+        return 0
+    elif not isinstance(lnk,Link):
+        return 1
+    else :
+        return deep_len(lnk.first) + deep_len(lnk.rest)
+
+def make_to_string(front, mid, back, empty_repr):
+    """ Returns a function that turns linked lists to strings.
+
+    >>> kevins_to_string = make_to_string("[", "|-]-->", "", "[]")
+    >>> jerrys_to_string = make_to_string("(", " . ", ")", "()")
+    >>> lst = Link(1, Link(2, Link(3, Link(4))))
+    >>> kevins_to_string(lst)
+    '[1|-]-->[2|-]-->[3|-]-->[4|-]-->[]'
+    >>> kevins_to_string(Link.empty)
+    '[]'
+    >>> jerrys_to_string(lst)
+    '(1 . (2 . (3 . (4 . ()))))'
+    >>> jerrys_to_string(Link.empty)
+    '()'
+    """
+    "*** YOUR CODE HERE ***"
+    
+    def analyze(lnk):
+        if lnk is Link.empty:
+            return empty_repr
+        elif lnk.first is not lnk.empty :
+            return front+str(lnk.first)+mid+analyze(lnk.rest)+back
+        #elif lnk.first is not lnk.empty and lnk.rest is lnk.empty:
+        #     return front+str(lnk.first)+mid+back+empty_repr
+
+
+    return analyze
+
+def tree_map(fn, t):
+    """Maps the function fn over the entries of t and returns the
+    result in a new tree.
+
+    >>> numbers = Tree(1,
+    ...                [Tree(2,
+    ...                      [Tree(3),
+    ...                       Tree(4)]),
+    ...                 Tree(5,
+    ...                      [Tree(6,
+    ...                            [Tree(7)]),
+    ...                       Tree(8)])])
+    >>> print(tree_map(lambda x: 2**x, numbers))
+    2
+      4
+        8
+        16
+      32
+        64
+          128
+        256
+    """
+    "*** YOUR CODE HERE ***"
+    t.label = fn(t.label) 
+    return Tree(t.label,[tree_map(fn,b) for b in t.branches])
+
+
+
+def add_trees(t1, t2):
+    """
+    >>> numbers = Tree(1,
+    ...                [Tree(2,
+    ...                      [Tree(3),
+    ...                       Tree(4)]),
+    ...                 Tree(5,
+    ...                      [Tree(6,
+    ...                            [Tree(7)]),
+    ...                       Tree(8)])])
+    >>> print(add_trees(numbers, numbers))
+    2
+      4
+        6
+        8
+      10
+        12
+          14
+        16
+    >>> print(add_trees(Tree(2), Tree(3, [Tree(4), Tree(5)])))
+    5
+      4
+      5
+    >>> print(add_trees(Tree(2, [Tree(3)]), Tree(2, [Tree(3), Tree(4)])))
+    4
+      6
+      4
+    >>> print(add_trees(Tree(2, [Tree(3, [Tree(4), Tree(5)])]), \
+    Tree(2, [Tree(3, [Tree(4)]), Tree(5)])))
+    4
+      6
+        8
+        5
+      5
+    """
+    "*** YOUR CODE HERE ***"
+    #if either tree is empty return another
+    if not t1:
+        return t2.copy_tree()
+    if not t2:
+        return t1.copy_tree()
+    #new tree
+    newt1 = t1.copy_tree()
+    newt2 = t2.copy_tree()
+    #anaylize every layer. if the two tree have different number of branches,then expand the low one to None.So their label can be added.
+    if len(newt1.branches) < len(newt2.branches):
+        newt1.branches+= [None for _ in range(len(newt1.branches),len(newt2.branches))]
+    if len(newt1.branches) > len(newt2.branches):
+        newt2.branches+= [None for _ in range(len(newt2.branches),len(newt1.branches))]
+    newtree_label = newt1.label + newt2.label
+    return Tree(newtree_label,[add_trees(newt1.branches[b],newt2.branches[b]) for b in range(len(newt1.branches))])
+
+
+
+# Link
+class Link:
+    """A linked list.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.first
+    1
+    >>> s.rest
+    Link(2, Link(3))
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is Link.empty:
+            return 'Link({})'.format(self.first)
+        else:
+            return 'Link({}, {})'.format(self.first, repr(self.rest))
+
+    def __str__(self):
+        """Returns a human-readable string representation of the Link
+
+        >>> s = Link(1, Link(2, Link(3, Link(4))))
+        >>> str(s)
+        '<1 2 3 4>'
+        >>> str(Link(1))
+        '<1>'
+        >>> str(Link.empty)  # empty tuple
+        '()'
+        """
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+# Tree
+class Tree:
+    def __init__(self, label, branches=[]):
+        for c in branches:
+            assert isinstance(c, Tree)
+        self.label = label
+        self.branches = list(branches)
+
+    def __repr__(self):
+        if self.branches:
+            branches_str = ', ' + repr(self.branches)
+        else:
+            branches_str = ''
+        return 'Tree({0}{1})'.format(self.label, branches_str)
+
+    def is_leaf(self):
+        return not self.branches
+
+    def __eq__(self, other):
+        return type(other) is type(self) and self.label == other.label \
+               and self.branches == other.branches
+    
+    def __str__(self):
+        def print_tree(t, indent=0):
+            tree_str = '  ' * indent + str(t.label) + "\n"
+            for b in t.branches:
+                tree_str += print_tree(b, indent + 1)
+            return tree_str
+        return print_tree(self).rstrip()
+
+    def copy_tree(self):
+        return Tree(self.label, [b.copy_tree() for b in self.branches])
---
+++ lab08/lab08_extra.py
@@ -0,0 +1,116 @@
+class Keyboard:
+    """A Keyboard takes in an arbitrary amount of buttons, and has a
+    dictionary of positions as keys, and values as Buttons.
+
+    >>> b1 = Button(0, "H")
+    >>> b2 = Button(1, "I")
+    >>> k = Keyboard(b1, b2)
+    >>> k.buttons[0].key
+    'H'
+    >>> k.press(1)
+    'I'
+    >>> k.typing([0, 1])
+    'HI'
+    >>> k.typing([1, 0])
+    'IH'
+    >>> b1.pressed
+    2
+    >>> b2.pressed
+    3
+    """
+
+    def __init__(self, *args):
+        "*** YOUR CODE HERE ***"
+
+    def press(self, info):
+        """Takes in a position of the button pressed, and
+        returns that button's output"""
+        "*** YOUR CODE HERE ***"
+
+    def typing(self, typing_input):
+        """Takes in a list of positions of buttons pressed, and
+        returns the total output"""
+        "*** YOUR CODE HERE ***"
+
+class Button:
+    def __init__(self, pos, key):
+        self.pos = pos
+        self.key = key
+        self.pressed = 0
+
+def make_advanced_counter_maker():
+    """Makes a function that makes counters that understands the
+    messages "count", "global-count", "reset", and "global-reset".
+    See the examples below:
+
+    >>> make_counter = make_advanced_counter_maker()
+    >>> tom_counter = make_counter()
+    >>> tom_counter('count')
+    1
+    >>> tom_counter('count')
+    2
+    >>> tom_counter('global-count')
+    1
+    >>> jon_counter = make_counter()
+    >>> jon_counter('global-count')
+    2
+    >>> jon_counter('count')
+    1
+    >>> jon_counter('reset')
+    >>> jon_counter('count')
+    1
+    >>> tom_counter('count')
+    3
+    >>> jon_counter('global-count')
+    3
+    >>> jon_counter('global-reset')
+    >>> tom_counter('global-count')
+    1
+    """
+    "*** YOUR CODE HERE ***"
+
+def trade(first, second):
+    """Exchange the smallest prefixes of first and second that have equal sum.
+
+    >>> a = [1, 1, 3, 2, 1, 1, 4]
+    >>> b = [4, 3, 2, 7]
+    >>> trade(a, b) # Trades 1+1+3+2=7 for 4+3=7
+    'Deal!'
+    >>> a
+    [4, 3, 1, 1, 4]
+    >>> b
+    [1, 1, 3, 2, 2, 7]
+    >>> c = [3, 3, 2, 4, 1]
+    >>> trade(b, c)
+    'No deal!'
+    >>> b
+    [1, 1, 3, 2, 2, 7]
+    >>> c
+    [3, 3, 2, 4, 1]
+    >>> trade(a, c)
+    'Deal!'
+    >>> a
+    [3, 3, 2, 1, 4]
+    >>> b
+    [1, 1, 3, 2, 2, 7]
+    >>> c
+    [4, 3, 1, 4, 1]
+    """
+    m, n = 1, 1
+
+    "*** YOUR CODE HERE ***"
+
+    if False: # change this line!
+        first[:m], second[:n] = second[:n], first[:m]
+        return 'Deal!'
+    else:
+        return 'No deal!'
+
+def zap(n):
+    i, count = 1, 0
+    while i <= n:
+        while i <= 5 * n:
+            count += i
+            print(i / 6)
+            i *= 3
+    return count
