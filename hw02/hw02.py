24d2d5356f9041410e956a001302235e65b743ea
XinyuJiang <503273921@qq.com>
2017-11-21 00:19:26 -0700

    homework 2

---
+++ hw02/hw02.py
@@ -0,0 +1,65 @@
+HW_SOURCE_FILE = 'hw02.py'
+
+from operator import add, mul
+
+def square(x):
+    return x * x
+
+def triple(x):
+    return 3 * x
+
+def identity(x):
+    return x
+
+def increment(x):
+    return x + 1
+
+def product(n, term):
+    """Return the product of the first n terms in a sequence.
+
+    n    -- a positive integer
+    term -- a function that takes one argument
+
+    >>> product(3, identity) # 1 * 2 * 3
+    6
+    >>> product(5, identity) # 1 * 2 * 3 * 4 * 5
+    120
+    >>> product(3, square)   # 1^2 * 2^2 * 3^2
+    36
+    >>> product(5, square)   # 1^2 * 2^2 * 3^2 * 4^2 * 5^2
+    14400
+    """
+    "*** YOUR CODE HERE ***"
+    a=term(1)
+    for i in range (2,n+1):
+        a=a*term(i)
+    return a
+# The identity function, defined using a lambda expression!
+identity = lambda k: k
+def factorial(n):
+    """Return n factorial for n >= 0 by calling product.
+
+    >>> factorial(4)
+    24
+    >>> factorial(6)
+    720
+    >>> from construct_check import check
+    >>> check(HW_SOURCE_FILE, 'factorial', ['Recursion', 'For', 'While'])
+    True
+    """
+    "*** YOUR CODE HERE ***"
+    return product(n,identity)
+
+def make_adder(n):
+    """Return a function that takes an argument K and returns N + K.
+
+    >>> add_three = make_adder(3)
+    >>> add_three(1) + add_three(2)
+    9
+    >>> make_adder(1)(2)
+    3
+    """
+    "*** YOUR CODE HERE ***"
+    def add_three(k):
+        return k+n
+    return lambda x: add_three(x)
