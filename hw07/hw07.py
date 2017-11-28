8bafeb53c7b3c278d0b3b2e5e3438aab15ab1cc7
XinyuJiang <503273921@qq.com>
2017-11-27 20:47:16 -0800

    Add files via upload

---
+++ hw07/hw07.py
@@ -0,0 +1,212 @@
+class Link:
+    """A linked list.
+
+    >>> s = Link(1, Link(2, Link(3)))
+    >>> s.first
+    1
+    >>> s.rest
+    Link(2, Link(3))
+    """
+    empty = ()
+
+    def __init__(self, first, rest=empty):
+        assert rest is Link.empty or isinstance(rest, Link)
+        self.first = first
+        self.rest = rest
+
+    def __repr__(self):
+        if self.rest is Link.empty:
+            return 'Link({})'.format(self.first)
+        else:
+            return 'Link({}, {})'.format(self.first, repr(self.rest))
+
+    def __str__(self):
+        """Returns a human-readable string representation of the Link
+
+        >>> s = Link(1, Link(2, Link(3, Link(4))))
+        >>> str(s)
+        '<1 2 3 4>'
+        >>> str(Link(1))
+        '<1>'
+        >>> str(Link.empty)  # empty tuple
+        '()'
+        """
+        string = '<'
+        while self.rest is not Link.empty:
+            string += str(self.first) + ' '
+            self = self.rest
+        return string + str(self.first) + '>'
+
+def digits(n):
+    """Return the digits of n as a linked list.
+
+    >>> digits(0) is Link.empty
+    True
+    >>> digits(543)
+    Link(5, Link(4, Link(3)))
+    """
+    s = Link.empty
+    while n > 0:
+        n, last = n // 10, n % 10
+        s = Link(last,s)
+
+    return s
+
+class Fib():
+    """A Fibonacci number.
+
+    >>> start = Fib()
+    >>> start
+    0
+    >>> start.next()
+    1
+    >>> start.next().next()
+    1
+    >>> start.next().next().next()
+    2
+    >>> start.next().next().next().next()
+    3
+    >>> start.next().next().next().next().next()
+    5
+    >>> start.next().next().next().next().next().next()
+    8
+    """
+    #previous = self.value 
+    def __init__(self, value=0):
+        self.value = value
+        self.pre1 = 0
+        self.pre2 = 1
+    def next(self):
+        t = self.pre1
+        self.pre1 = self.pre2
+        self.pre2 = self.pre1+t
+        self.value = self.pre1
+        return self;
+
+    def __repr__(self):
+        self.pre1 = 0#every time output,initialize
+        self.pre2 = 1
+        return str(self.value)
+
+class VendingMachine:
+    """A vending machine that vends some product for some price.
+
+    >>> v = VendingMachine('candy', 10)
+    >>> v.vend()
+    'Machine is out of stock.'
+    >>> v.deposit(15)
+    'Machine is out of stock. Here is your $15.'
+    >>> v.restock(2)
+    'Current candy stock: 2'
+    >>> v.vend()
+    'You must deposit $10 more.'
+    >>> v.deposit(7)
+    'Current balance: $7'
+    >>> v.vend()
+    'You must deposit $3 more.'
+    >>> v.deposit(5)
+    'Current balance: $12'
+    >>> v.vend()
+    'Here is your candy and $2 change.'
+    >>> v.deposit(10)
+    'Current balance: $10'
+    >>> v.vend()
+    'Here is your candy.'
+    >>> v.deposit(15)
+    'Machine is out of stock. Here is your $15.'
+
+    >>> w = VendingMachine('soda', 2)
+    >>> w.restock(3)
+    'Current soda stock: 3'
+    >>> w.restock(3)
+    'Current soda stock: 6'
+    >>> w.deposit(2)
+    'Current balance: $2'
+    >>> w.vend()
+    'Here is your soda.'
+    """
+    "*** YOUR CODE HERE ***"
+    def __init__(self, stuff,value):
+        self.stuff = stuff
+        self.stock = 0
+        self.value = value
+        self.balance = 0
+
+    def vend(self):
+        if self.stock == 0:
+            return 'Machine is out of stock.'
+        elif self.balance > self.value:
+            out = self.balance - self.value
+            self.stock-=1
+            self.balance = 0
+            return 'Here is your {0} and ${1} change.'.format(self.stuff, out) 
+        elif self.balance == self.value:
+            self.stock-=1
+            self.balance = 0
+            return 'Here is your {0}.'.format(self.stuff) 
+        return 'You must deposit ${0} more.'.format(self.value - self.balance)
+
+
+
+    def restock(self,temp):
+        self.stock+=temp
+        return 'Current {0} stock: {1}'.format(self.stuff, self.stock)
+    def deposit(self,temp):
+        tempin = self.balance+temp
+        if self.stock ==0:
+            return 'Machine is out of stock. Here is your ${0}.'.format(tempin)
+        self.balance = tempin
+        return 'Current balance: ${0}'.format(self.balance)
+
+
+
+
+
+class MissManners:
+    """A container class that only forwards messages that say please.
+
+    >>> v = VendingMachine('teaspoon', 10)
+    >>> v.restock(2)
+    'Current teaspoon stock: 2'
+
+    >>> m = MissManners(v)
+    >>> m.ask('vend')
+    'You must learn to say please first.'
+    >>> m.ask('please vend')
+    'You must deposit $10 more.'
+    >>> m.ask('please deposit', 20)
+    'Current balance: $20'
+    >>> m.ask('now will you vend?')
+    'You must learn to say please first.'
+    >>> m.ask('please hand over a teaspoon')
+    'Thanks for asking, but I know not how to hand over a teaspoon.'
+    >>> m.ask('please vend')
+    'Here is your teaspoon and $10 change.'
+
+    >>> double_fussy = MissManners(m) # Composed MissManners objects
+    >>> double_fussy.ask('deposit', 10)
+    'You must learn to say please first.'
+    >>> double_fussy.ask('please deposit', 10)
+    'Thanks for asking, but I know not how to deposit.'
+    >>> double_fussy.ask('please please deposit', 10)
+    'Thanks for asking, but I know not how to please deposit.'
+    >>> double_fussy.ask('please ask', 'please deposit', 10)
+    'Current balance: $10'
+    """
+    def __init__(self, obj):
+        self.obj = obj
+
+    def ask(self, message, *args):
+        magic_word = 'please '
+        if not message.startswith(magic_word):
+            return 'You must learn to say please first.'
+        method = message[7:]
+        #if len(args)>=2:
+        #    print (self.obj)
+        #    print (args)
+        if not hasattr(self.obj, method):
+            #return 'Thanks for asking, but I know not how to {0}.'.format(message.split()[1:])
+            return 'Thanks for asking, but I know not how to {0}.'.format(method)
+        else:
+            return getattr(self.obj, method)(*args)
+        
